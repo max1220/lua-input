5a9d2cb388ecca7fa2372914a08a3de395269caa
max1220 <max1220@users.noreply.github.com>
2017-03-22 19:31:16 +0100

    Implemented multi_read, cleanup

--- input.c
+++ input.c
@@ -18,8 +18,8 @@
 #define LUA_T_PUSH_S_CF(S, CF) lua_pushstring(L, S); lua_pushcfunction(L, CF); lua_settable(L, -3);
 
 typedef struct {
-  int fd;
-  char *path;
+	int fd;
+	char *path;
 } input_t;
 
 
@@ -28,12 +28,12 @@ typedef struct {
 static int l_input_read(lua_State *L) {
 	input_t *input = (input_t *)lua_touserdata(L, 1);
 	struct input_event data;
-	
+
 	if(read(input->fd, &data, sizeof(data)) > 0) {
-  
+
 		// printf("fd: %d\n", input->fd);
 		// printf("C:     time: %x, type: %x, code: %x, value: %x\n", data.time.tv_sec, data.type, data.value, data.code);
-		
+
 		lua_newtable(L);
 		LUA_T_PUSH_S_N("time", (double) data.time.tv_sec)
 		LUA_T_PUSH_S_N("utime", (double) data.time.tv_usec)
@@ -43,7 +43,7 @@ static int l_input_read(lua_State *L) {
 		LUA_T_PUSH_S_N("value", data.value)
 		return 1;
 	}
-	
+
 	return 0;
 }
 
@@ -58,110 +58,127 @@ static int l_input_tostring(lua_State *L) {
 
 static int l_open(lua_State *L) {
 	input_t *dev = (input_t *)lua_newuserdata(L, sizeof(*dev));
-	
+
 	dev->path = strdup(luaL_checkstring(L, 1));
-	
+
 	if (lua_toboolean(L, 2)) {
 		dev->fd = open(dev->path, O_RDONLY | O_NONBLOCK);
 	} else {
 		dev->fd = open(dev->path, O_RDONLY);
 	}
-	
+
 	if(dev->fd < 0) {
 		lua_pushnil(L);
 		lua_pushfstring(L, "Can't open device!");
 		return 2;
 	}
-	
+
 	lua_createtable(L, 0, 9);
 	lua_pushvalue(L, -1);
 	lua_setfield(L, -2, "__index");
-  
+
 	LUA_T_PUSH_S_CF("read", l_input_read)
 	//LUA_T_PUSH_S_CF("close", l_input_close)
 	//LUA_T_PUSH_S_CF("__gc", l_input_close)
 	LUA_T_PUSH_S_CF("__tostring", l_input_tostring)
 	lua_setmetatable(L, -2);
-	
+
 	return 1;
+}
+
+static int l_read_multiple(lua_State *L) {
+	int count = lua_gettop(L);
+	input_t *dev;
+	struct input_event data;
+	int i;
+	for (i=1; i<=count; i=i+1) {
+		dev = (input_t *)lua_touserdata(L, i);
+		if(read(dev->fd, &data, sizeof(data)) > 0) {
+			lua_newtable(L);
+			LUA_T_PUSH_S_N("time", (double) data.time.tv_sec)
+			LUA_T_PUSH_S_N("utime", (double) data.time.tv_usec)
+			LUA_T_PUSH_S_N("type", data.type)
+			LUA_T_PUSH_S_N("code", data.code)
+			LUA_T_PUSH_S_N("value", data.value)
+		} else {
+			lua_pushnil(L);
+		}
+	}
+
+	return count;
 }
 
 
 static int l_list(lua_State *L) {
 	FILE *fp;
-  char *line = NULL;
-  size_t len = 0;
-  
-  int index = 1;
-  
-  int bus = 0;
-  int vendor = 0;
-  int product = 0;
-  int version = 0;
-  char name[255];
-  char phys[255];
-  char sysfs[255];
-  char handlers[255];
-  
-  
+	char *line = NULL;
+	size_t len = 0;
 
-  fp = fopen("/proc/bus/input/devices", "r");
-  if (fp == NULL) {
+	int index = 1;
+	int bus = 0;
+	int vendor = 0;
+	int product = 0;
+	int version = 0;
+	char name[255];
+	char phys[255];
+	char sysfs[255];
+	char handlers[255];
+
+	fp = fopen("/proc/bus/input/devices", "r");
+	if (fp == NULL) {
 		lua_pushnil(L);
 		lua_pushstring(L, "Can't open /proc/bus/input/devices");
 		return 2;
-  }
- 
-  lua_newtable(L);
-  //lua_pushnumber(L, index);
-  //lua_newtable(L);
-  
-  while (getline(&line, &len, fp) != -1) {
+	}
+
+	lua_newtable(L);
+
+	while (getline(&line, &len, fp) != -1) {
 		if (strlen(line) - 1 > 0) {
 			line[strlen(line)-1] = '\0';
 			if (line[0] == 'I') {
 				sscanf(line, "I: Bus=%4x Vendor=%4x Product=%4x Version=%4x", &bus, &vendor, &product, &version);
 			} else if (line[0] == 'N') {
-				sscanf(line, "N: Name=\"%s\"", name);
+				sscanf(line, "N: Name=\"%254c", name);
+				phys[strlen(line) - strlen("N: Name=\"") - 2] = '\0';
 			} else if (line[0] == 'P') {
-				sscanf(line, "P: Phys=%s", phys);
+				sscanf(line, "P: Phys=%254c", phys);
+				phys[strlen(line) - strlen("P: Phys=") - 1] = '\0';
 			} else if (line[0] == 'S') {
-				sscanf(line, "S: Sysfs=%s", sysfs);
+				sscanf(line, "S: Sysfs=%254c", sysfs);
+				sysfs[strlen(line) - strlen("S: Sysfs=") - 1] = '\0';
 			} else if (line[0] == 'H') {
 				sscanf(line, "H: Handlers=%254c", handlers);
-				// w = strlen(line) - strlen("H: Handlers=")
 				handlers[strlen(line) - strlen("H: Handlers=") - 1] = '\0';
 			}
 		} else {			
 			lua_pushnumber(L, index);
 			lua_newtable(L);
-			
+
 			LUA_T_PUSH_S_S("handlers", handlers)
 			LUA_T_PUSH_S_S("sysfs", sysfs)
 			LUA_T_PUSH_S_S("phys", phys)
 			LUA_T_PUSH_S_S("name", name)
-			
+
 			lua_pushstring(L, "id");
 			lua_newtable(L);
-			
+
 			LUA_T_PUSH_S_N("bus", bus)
 			LUA_T_PUSH_S_N("vendor", vendor)
 			LUA_T_PUSH_S_N("product", product)
 			LUA_T_PUSH_S_N("version", version)
-				
+
 			lua_settable(L, -3);
-			
+
 			lua_settable(L, -3);
-			
+
 			index = index + 1;
 		}
-  }
-  
-  //lua_settable(L, -3);
+	}
 
-  fclose(fp);
-  free(line);
-  
+	fclose(fp);
+	free(line);
+
 	return 1;
 }
 
@@ -175,15 +192,10 @@ int luaopen_input(lua_State *L) {
 		//{"open_blocking", l_open_non_blocking},
 		{"open", l_open},
 		{"list", l_list},
+		{"read_multiple", l_read_multiple},
 		{NULL, NULL}
 	};
 
 	luaL_openlib(L, "input", functions, 0);
 	return 0;
 }
-
-
-
-
-//
-
