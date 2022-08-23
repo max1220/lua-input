a757fa5363eec8a25d4eedfb40ad14e3ecc5cf9c
Max <max1220@users.noreply.github.com>
2022-08-23 14:02:23 +0200

    Added support for printing capabilities

--- doc/examples/list_devices.lua
+++ doc/examples/list_devices.lua
@@ -2,7 +2,10 @@
 local input = require("lua-input")
 
 -- get list of devices and info
-local devices = input.get_devices_list()
+local devices = input.get_devices()
+
+-- parse -v/--verbose command line argument
+local verbose = (arg[1]=="-v") or (arg[1]=="--verbose")
 
 -- show list
 for i,device in pairs(devices) do
@@ -13,5 +16,19 @@ for i,device in pairs(devices) do
 	print("","Handlers:")
 	for k,v in pairs(device.handlers) do
 		print("","",k,v)
+	end
+	print("","Capabilities:")
+	for cap_type,caps in pairs(device.capabilities) do
+		print("","","cap_type:", cap_type)
+		if verbose then
+			local cap_names = {}
+			for cap_name, enabled in pairs(caps) do
+				if enabled then
+					table.insert(cap_names, "\t\t\t"..cap_name)
+				end
+			end
+			table.sort(cap_names)
+			print(table.concat(cap_names, "\n"))
+		end
 	end
 end
