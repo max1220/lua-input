b426b2d47ee58ec0daf863c387eecaf338507ee9
Max <max1220@users.noreply.github.com>
2022-08-23 14:05:40 +0200

    Fix some bugs, improved readabillity

--- doc/examples/type_text.lua
+++ doc/examples/type_text.lua
@@ -1,7 +1,7 @@
 #!/usr/bin/env lua5.1
 local input = require("lua-input")
 
--- sleep for specified amount of seconds
+-- function to sleep for specified amount of seconds
 local sleep = require("time").sleep
 
 
@@ -21,7 +21,7 @@ end
 -- enable the needed event bits
 local function set_bits(device)
 	-- enable the EV_KEY events for the device
-	input:set_bit("EVBIT", input.event_codes.EV_KEY)
+	device:set_bit("EVBIT", input.event_codes.EV_KEY)
 
 	-- enable the supported key values
 	for _,value in pairs(text_keys) do
@@ -29,16 +29,11 @@ local function set_bits(device)
 	end
 end
 
--- write the SYN_REPORT event to the device
-local function write_syn(device)
-	device:write_event(input.event_codes.EV_SYN,input.event_codes.SYN_REPORT,0)
-end
-
 -- write a single key down/key up event to the device based on the character(upper-case)
 local function write_event_from_character(device, char, down)
 	local key = text_keys[char]
 	device:write_event(input.event_codes.EV_KEY, key, down and 1 or 0)
-	write_syn(device)
+	device:write_event(input.event_codes.EV_SYN,input.event_codes.SYN_REPORT,0)
 end
 
 -- convert a single character to a series of key down/key up events,
@@ -60,7 +55,7 @@ local function write_character(device, char, delay)
 	-- write key up
 	write_event_from_character(device, char_upper, false)
 
-	if shift then
+	if need_shift then
 		-- write shift up
 		write_event_from_character(device, "shift", false)
 	end
@@ -79,15 +74,18 @@ local input_device = input.open_input("uinput")
 -- setup the device parameters and create a device
 input_device:dev_setup("lua-input test", 0x1234, 0x5678, false)
 
+-- register the needed event codes
+set_bits(input_device)
+
 -- wait for userland to become ready
-time.sleep(1)
+sleep(1)
 
 -- type a string of text by pressing the corresponding keys
 local text = arg[1] or "Hello from lua-input"
 type_text(input_device, text, 0.1)
 
 -- wait for userland to handle all outstanding events
-time.sleep(1)
+sleep(1)
 
 -- detroy input
---input:close()
+input:close()
